cc6b2451cb50e6f3cbe967681fe6d43bf2c31401
loveiscomingsoon <loveiscomingsoon@users.noreply.github.com>
2026-06-25 21:35:49 +0700

    Adjust cover cat animations

--- books/pocket-book.js
+++ books/pocket-book.js
@@ -70,7 +70,7 @@ const pages = [
     html: () => `
       <div class="cover-art-wrap">
         <img class="cover-art" src="${COVER_ART}" alt="ภาพปกธรรมะวันนี้">
-        <img class="moving-cat cover-cat" src="../assets/daily-art/cover-cat-sticker.png?v=20260625-2" alt="" aria-hidden="true">
+        <img class="cover-cat cover-cat-tilt" src="../assets/daily-art/cover-cat-cutout.png?v=20260625-1" alt="" aria-hidden="true">
       </div>
       <p class="page-kicker">ธรรมะสำหรับชีวิตประจำวัน</p>
       <h1>ธรรมะพกพา</h1>
